85740efa5d748d2bc82a9a1ce15572fe3037f915
iDougas <38037625+iDougas@users.noreply.github.com>
2021-04-29 00:15:28 +0300

    gaming
    gaming

--- gaming.cpp
+++ gaming.cpp
@@ -1,17 +1,29 @@
 #include<iostream>
+#include<fstream>
 #include<string>
 using namespace std;
 
 void Gaming(string game);
+void VideoGaming(string game;)
 
 int main()
 {
   string game = "gaming";
   Gaming(game);
+  VideoGaming(game);
   return 0;
 }
 
 void Gaming(string game)
 {
   cout << game;
+}
+
+void VideoGaming(string game)
+{
+  ofstream gamer("gaming.txt");
+  for(int i = 0; i < 6; i++) {
+    gamer << game[i];
+  }
+  gamer.close();
 }
