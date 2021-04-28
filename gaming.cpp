21450b0bf4e06dd7ae39ab03c790c7603d88022b
iDougas <38037625+iDougas@users.noreply.github.com>
2021-04-29 00:03:55 +0300

    Create gaming.cpp

---
+++ gaming.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void Gaming(string game);
+
+int main()
+{
+  string game = "gaming";
+  Gaming(game);
+  return 0;
+}
+
+void Gaming(string game)
+{
+  cout << game;
+}
