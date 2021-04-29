2fcf89dae46e97c621292414799f94c9ea4c07f5
iDougas <38037625+iDougas@users.noreply.github.com>
2021-04-29 08:45:36 +0300

    gaming

--- gaming.cpp
+++ gaming.cpp
@@ -3,14 +3,19 @@
 #include<string>
 using namespace std;
 
+struct Ultra {
+  string gaming = "time";
+};
+
 void Gaming(string game);
-void VideoGaming(string game);
+void VideoGaming(string game, Ultra epic);
 
 int main()
 {
   string game = "gaming";
   Gaming(game);
-  VideoGaming(game);
+  Ultra epic;
+  VideoGaming(game, epic);
   return 0;
 }
 
@@ -23,8 +28,13 @@ void VideoGaming(string game)
 {
   ofstream gamer("gaming.txt");
   int bro = game.length();
+  int how = epic.gaming.length();
   for(int i = 0; i < bro; i++) {
     gamer << game[i];
+  }
+  gamer << " ";
+  for(int i = 0; i < how; i++) {
+    gamer << epic.gaming[i];
   }
   gamer.close();
 }
